ceb8ce7c18b514840a7db06561dcf802d430255b
‘me’ <me@deerfriend10@gmail.com>
2026-01-02 20:40:46 +0400

    Implement function to pass initial test

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
+    let strings = '';
+    for (let i = 0; i < num; i++) {
+        strings += str;
+    }
 
+    return strings;
 };
 
 // Do not edit below this line
