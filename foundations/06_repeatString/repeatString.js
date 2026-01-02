295765ece87f7d31ed84ceeccb2f8c0cdce154af
‘me’ <me@deerfriend10@gmail.com>
2026-01-02 20:48:39 +0400

    Handle negative number edge case

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,4 +1,9 @@
 const repeatString = function(str, num) {
+    
+    if ( num < 0 ) {
+        return 'ERROR';
+    }
+
     let strings = '';
     for (let i = 0; i < num; i++) {
         strings += str;
