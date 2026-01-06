6a58928c0f6e92088c5e828934f1dacf8c45e847
‘me’ <me@deerfriend10@gmail.com>
2026-01-06 18:23:55 +0400

    Implement basic sum between two integers

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,4 +1,24 @@
-const sumAll = function() {
+const sumAll = function(int1, int2) {
+
+    if (!Number.isInteger(int1) || !Number.isInteger(int2)) return "Only whole numbers are accepted.";
+
+    if (int1 < 0 || int2 < 0) return "Only positive integers are accepted";
+
+    const arr = [];
+
+    if (int1 < int2) {
+        for (let i = int1; i <= int2; i++) {
+            arr.push(i);
+        }
+    } else if (int1 > int2) {
+        for (let i = int1; i >= int2; i--) {
+            arr.push(i);
+        }
+    } else if (int1 === int2) {
+        arr.push(int1);
+    }
+
+    return arr.reduce( (accum, currentItem) => accum + currentItem, 0);
 
 };
 
