0aa56138aab799d75e6ab8e86b2905cc8b2131f5
‘me’ <me@deerfriend10@gmail.com>
2026-01-06 19:21:55 +0400

    Implement basic leap year validation

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,4 +1,16 @@
-const leapYears = function() {
+const leapYears = function(year) {
+
+    const isCentury = year % 100 == 0;
+
+    if ( isCentury && year % 400 === 0 ) {
+        return true;
+    } else if ( isCentury && !year % 400 === 0 ) {
+        return false;
+    } else if ( !isCentury && year % 4 === 0)  {
+        return true;
+    } else if ( !isCentury && !year % 4 === 0 ) {
+        return false;
+    }
 
 };
 
